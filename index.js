04469fdab1a9544f63fe5505d633378a0762ee6a
Sandra G <sandyjtech@gmail.com>
2023-06-19 16:21:35 -0700

    created ruduce method

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let totalBatteries = 0;
 
-// Code your solution here
+function reducer(accumulator, item) {
+  return accumulator + item;
+}
+
+function batteries() {
+  totalBatteries = batteryBatches.reduce(reducer, totalBatteries);
+  return totalBatteries;
+}
+console.log(batteries());
